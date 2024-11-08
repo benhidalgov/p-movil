3bd6236b4efde1d371062e76c9a76693b748a6a2
Benjamin Hidalgo <ben.hidalgov@duocuc.cl>
2024-11-07 23:11:15 -0300

    aa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,44 @@
-import { Routes } from '@angular/router';
+import { Routes, CanActivateFn } from '@angular/router';
+import { loginGuard } from './guards/login.guard';
+import { homeGuard } from './guards/home.guard';
+import { Ingreso } from './model/ingreso';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage),
+  },
+  {
+    path: 'theme',
+    loadComponent: () => import('./pages/theme/theme.page').then( m => m.ThemePage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/inicio/inicio.page').then( m => m.InicioPage)
+  },
+  {
+    path: 'correo',
+    loadComponent: () => import('./pages/correo/correo.page').then( m => m.CorreoPage)
+  },
+  {
+    path: 'pregunta',
+    loadComponent: () => import('./pages/pregunta/pregunta.page').then( m => m.PreguntaPage)
+  },
+  {
+    path: 'correcto',
+    loadComponent: () => import('./pages/correcto/correcto.page').then( m => m.CorrectoPage)
+  },
+  {
+    path: 'incorrecto',
+    loadComponent: () => import('./incorrecto/incorrecto.page').then( m => m.IncorrectoPage)
+  },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./pages/inicio/inicio.page').then( m => m.InicioPage)
   },
 ];
